e2bcbd82c2e1c63d5f5a68e92baac4344377cd11
John Everett <everjohn@byu.edu>
2025-11-03 17:00:43 -0700

    fixed some bugs on mobile

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 lucide.createIcons();
+
+document.addEventListener('DOMContentLoaded', function () {
+    const mobileMenuButton = document.getElementById('mobile-menu-button');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    mobileMenuButton.addEventListener('click', function () {
+        mobileMenu.classList.toggle('hidden');
+    });
+});
